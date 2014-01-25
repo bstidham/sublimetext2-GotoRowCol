01d15e38562ef727c681dba08c8dd5c917a9001b
Bill Stidham <billstidham@gmail.com>
2014-01-24 21:31:39 -0500

    r1.01
    https://github.com/bstidham/sublimetext2-GotoRowCol/issues/1
    
    Parenthesized print for compatibility: 
    print without parens is only supported in sublime text 2 (python 2). sublime text 3 (python 3) requires that print be parenthesized.

--- GotoRowCol.py
+++ GotoRowCol.py
@@ -27,16 +27,16 @@ class PromptGotoRowColCommand(sublime_plugin.WindowCommand):
 
 class GotoRowColCommand(sublime_plugin.TextCommand):
 	def run(self, edit, row, col):
-		print "INFO: Input: " + str({"row": row, "col": col})
+		print("INFO: Input: " + str({"row": row, "col": col}))
 		# rows and columns are zero based, so subtract 1
 		# convert text to int
 		(row, col) = (int(row) - 1, int(col) - 1)
 		if row > -1 and col > -1:
 			# col may be greater than the row length
 			col = min(col, len(self.view.substr(self.view.full_line(self.view.text_point(row, 0))))-1)
-			print "INFO: Calculated: " + str({"row": row, "col": col})
+			print("INFO: Calculated: " + str({"row": row, "col": col}))
 			self.view.sel().clear()
 			self.view.sel().add(sublime.Region(self.view.text_point(row, col)))
 			self.view.show(self.view.text_point(row, col))
 		else:
-			print "ERROR: row or col are less than zero"
+			print("ERROR: row or col are less than zero")
